58ae53ae386cc28f907716a9f74617c7b3e2f7ca
DorahRotich <rotichkurgat@gmail.com>
2022-09-28 13:40:41 -0700

    corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,4 +1,7 @@
-t_score(a_dictionary):
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
     """Returns a key with the biggest integer value."""
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
